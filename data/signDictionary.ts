990a5258a4ef7d57f57c5d4581ac9fe877036891
linowo <nnf231363469@gmail.com>
2026-09-13 00:19:18 +0800

    docs: complete metadata and media for all 6 trained sign classes

--- data/signDictionary.ts
+++ data/signDictionary.ts
@@ -1,66 +1,58 @@
 export const signDictionary = {
-    /*
-    : {
-    word: '',   // 詞本身
-    // 動作描述
-    movement: '向外移動',
-    // 示範影片
-    video: '.mp4',
-    // 手型圖片
-    ＨandshapeImage: [
-      '/Handshape/1.1.1.jpg'
-    ],
-    // 位置圖片
-    positionImage: [
-      '/position/1.1額頭.jpg'
-    ]
-  },    */
   你: {
-    word: '你',  
-    
+    word: '你',
     movement: '一手食指指向對方。',
     video: '/video/you.mp4',
-    ＨandshapeImage: [
-      '/Handshape/1.1.3.jpg'
-    ],
-    positionImage: [
-      '/position/4.1身前.jpg'
-    ]
-},
-    吃飯: {
-    word: '吃飯',  
-    
-    movement: '一手在嘴巴前做扒飯狀，另一手接近下巴做拿碗狀。',
-    video: '/video/eat.mp4',
-    ＨandshapeImage: [
-      '/Handshape/5.3.2.jpg'
-    ],
-    positionImage: [
-      '/position/5.6掌心.jpg'
-    ]
-},
-    我: {
-    word: '我',  
-    
+    ＨandshapeImage: ['/Handshape/1.1.3.jpg'],
+    positionImage: ['/position/4.1身前.jpg']
+  },
+  我: {
+    word: '我',
     movement: '一手食指觸鼻尖。',
     video: '/video/me.mp4',
-    ＨandshapeImage: [
-      '/Handshape/1.1.3.jpg'
-    ],
-    positionImage: [
-      '/position/1.4鼻子.jpg'
-    ]
-},
-    有沒有: {
-    word: '有沒有',  
-    
+    ＨandshapeImage: ['/Handshape/1.1.3.jpg'],
+    positionImage: ['/position/1.4鼻子.jpg']
+  },
+  吃飯: {
+    word: '吃飯',
+    movement: '一手在嘴巴前做扒飯狀，另一手接近下巴做拿碗狀。',
+    video: '/video/eat.mp4',
+    ＨandshapeImage: ['/Handshape/5.3.2.jpg'],
+    positionImage: ['/position/5.6掌心.jpg']
+  },
+  吃: {
+    word: '吃',
+    movement: '一手在嘴巴前做扒飯狀，另一手接近下巴做拿碗狀。',
+    video: '/video/eat.mp4',
+    ＨandshapeImage: ['/Handshape/5.3.2.jpg'],
+    positionImage: ['/position/5.6掌心.jpg']
+  },
+  完成: {
+    word: '完成',
+    movement: '雙手掌心朝下，一手從另一手下方移到其手背上。',
+    video: 'https://twtsl.ccu.edu.tw/video/a/accomplish.mp4',
+    ＨandshapeImage: ['https://twtsl.ccu.edu.tw/img/handshapeList/503_九a.jpg'],
+    positionImage: ['https://twtsl.ccu.edu.tw/img/locationList/604_back_of_hand.jpg']
+  },
+  有沒有: {
+    word: '有沒有',
     movement: '一手掌心向前，重複開合。',
     video: '/video/have_not_have.mp4',
-    ＨandshapeImage: [
-      '/Handshape/5.8.jpg'
-    ],
-    positionImage: [
-      '/position/4.1身前.jpg'
-    ]
-}
+    ＨandshapeImage: ['/Handshape/5.8.jpg'],
+    positionImage: ['/position/4.1身前.jpg']
+  },
+  還沒: {
+    word: '還沒',
+    movement: '一手掌心朝內，四指向下，左右幌動。',
+    video: 'https://twtsl.ccu.edu.tw/video/n/not_yet.mp4',
+    ＨandshapeImage: ['https://twtsl.ccu.edu.tw/img/handshapeList/502_手.jpg'],
+    positionImage: ['https://twtsl.ccu.edu.tw/img/locationList/401_in_front_of_body.jpg']
+  },
+  還沒有: {
+    word: '還沒有',
+    movement: '一手掌心朝內，四指向下，左右幌動。',
+    video: 'https://twtsl.ccu.edu.tw/video/n/not_yet.mp4',
+    ＨandshapeImage: ['https://twtsl.ccu.edu.tw/img/handshapeList/502_手.jpg'],
+    positionImage: ['https://twtsl.ccu.edu.tw/img/locationList/401_in_front_of_body.jpg']
+  }
 }
